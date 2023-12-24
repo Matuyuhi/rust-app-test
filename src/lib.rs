252663da24cf8da26aca3237a4477b472a4a1cec
Matuyuhi <Matuyuhi@users.noreply.github.com>
2023-12-24 12:01:23 +0000

    deploy: c943ea58cd9c60570ed75fb156622e38d3a5fdcd

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+mod utils;
+
+use std::ffi::c_int;
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+extern {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+    alert("Hello, wasm!");
+}
+
+#[wasm_bindgen]
+pub fn calculate(a: c_int, b:c_int) -> c_int {
+    return a+b;
+}
